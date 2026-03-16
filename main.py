5ad30e7b1d8b417696be40c45223bf454188e886
tamthanh <ngthtam10012005@gmail.com>
2026-03-17 00:51:20 +0700

    Set up qdrant

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams
+
+
+# client = QdrantClient(url="http://localhost:6333")
+# client = cr
+def main():
+    pass
+    # response = requests.post(
+    #     "http://localhost:11434/api/embed",
+    #     json={"model": "mxbai-embed-large", "input": "Hello, world!"},
+    # )
+    # print(response)
+
+if __name__ == "__main__":
+    main()
